5fbaf69cde3a55ec1d964518eb403524b1b84ba6
amoshagay <amos@terasky.com>
2024-06-30 16:19:50 +0300

    Update test_sow_generator.py

--- test_sow_generator.py
+++ test_sow_generator.py
@@ -4,11 +4,11 @@ from streamlit.testing.v1 import AppTest
 class TestChatbotApp(unittest.TestCase):
     def test_smoke(self):
         """Test if the app runs without throwing an exception."""
-        at = AppTest.from_file("../sow_generator.py", default_timeout=10).run()
+        at = AppTest.from_file("sow_generator.py", default_timeout=10).run()
         assert not at.exception
 
 
     def test_sidebar(self):
         """Test if a single text input exists in the sidebar."""
-        at = AppTest.from_file("../sow_generator.py").run()
+        at = AppTest.from_file("sow_generator.py").run()
         assert len(at.text_input) == 2
